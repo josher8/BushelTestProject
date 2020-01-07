153252b0c34c6c050f6b0dd74b56875861660808
Josh Slebodnik <jslebodnik@bushelpowered.com>
2020-01-07 16:07:28 -0600

    Added Token Model

---
+++ BushelTestProject/Models/Token.swift
@@ -0,0 +1,14 @@
+//
+//  Token.swift
+//  BushelTestProject
+//
+//  Created by Josh Slebodnik on 1/7/20.
+//  Copyright © 2020 Josh Slebodnik. All rights reserved.
+//
+
+import Foundation
+import EVReflection
+
+class Token: EVObject {
+    var token: String = ""
+}
